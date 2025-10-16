27c0c6bbf2121129849e60db6873a6069e7fd7ac
seiya1998 <fukushi1998@gmail.com>
2025-10-17 02:20:29 +0900

    fix: :lady_beetle: タブの実装

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
@@ -14,20 +15,41 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarStyle: {
+          shadowRadius: 0.5,
+          shadowOpacity: 0.03,
+          borderTopWidth: 0,
+          shadowColor: '#000',
+          paddingTop: 6
+        },
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'ホーム',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="likes"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'いいね',
+          tabBarIcon: ({ color }) => <EvilIcons size={32} name="like" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: 'メッセージ',
+          tabBarIcon: ({ color }) => <AntDesign size={22} name="message" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="mypages"
+        options={{
+          title: 'マイページ',
+          tabBarIcon: ({ color }) => <EvilIcons size={32} name="user" color={color} />,
         }}
       />
     </Tabs>
