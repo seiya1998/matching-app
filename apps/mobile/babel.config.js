824d1e880c9f562b9f4fb1ae3e9ec886ed8e1e33
seiya1998 <fukushi1998@gmail.com>
2025-10-17 01:55:36 +0900

    fix: :lady_beetle: tailwindcssの設定を修正

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
     plugins: [
       [
         'module-resolver',
@@ -11,7 +14,6 @@ module.exports = function (api) {
           }
         }
       ],
-      'nativewind/babel'
     ]
   };
 };
